f6d08d2c4c90a714f0bc8c99fb8d48e8ec55080e
Carlos San Martin <carlosanmartin@gmail.com>
2018-03-06 19:39:51 +0100

    updated

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { HttpModule } from '@angular/http';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { SessionService } from '../services/session.service';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { MapComponent } from './map/map.component';
-import { CatalogComponent } from './catalog/catalog.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, ApplicationRef } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HttpModule } from "@angular/http";
+import { LoginFormComponent } from "./login-form/login-form.component";
+import { SessionService } from "../services/session.service";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { MapComponent } from "./map/map.component";
+import { CatalogComponent } from "./catalog/catalog.component";
+
+import { AgmCoreModule } from "@agm/core";
 
 const routes: Routes = [
   // { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '',  component: HomeComponent },
-  { path: 'login',  component: LoginFormComponent },
-  { path: 'map', component: MapComponent },
-  { path: 'catalog', component: CatalogComponent },
+  { path: "", component: HomeComponent },
+  { path: "login", component: LoginFormComponent },
+  { path: "map", component: MapComponent },
+  { path: "catalog", component: CatalogComponent }
 ];
 
 @NgModule({
@@ -28,11 +31,15 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     HttpModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyAcTIMzYcZ8UhOpehf0rQfD-aZSbIfZYQ0"
+    })
   ],
   providers: [SessionService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- client/src/app/map/map.component.ts
+++ client/src/app/map/map.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  public lat: number = 51.678418;
+  public lng: number = 7.809007;
+
   constructor() { }
 
   ngOnInit() {
