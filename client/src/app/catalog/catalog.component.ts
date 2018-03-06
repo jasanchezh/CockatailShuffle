dc80c5e05aa70bca09d63f18b7e1b74dc4682491
Jose Sanchez <joseansanher@gmail.com>
2018-03-06 13:59:25 +0100

    mixing drinks

--- client/src/app/catalog/catalog.component.ts
+++ client/src/app/catalog/catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 
 @Component({
@@ -11,28 +12,31 @@ export class CatalogComponent implements OnInit {
   public ronImg: string;
   public vodkaImg: string;
   public ginebraImg: string;
-  public src: string;
-
+  public srcImg: string;
+  public drink: string;
 
   constructor() {
-    this.src = 'https://static.vix.com/es/sites/default/files/imj/elgrancatador/c/cocteles_fresa5.jpg';
+    
     this.ronImg = 'https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201612/28/00118733302030____2__600x600.jpg';
     this.vodkaImg = 'https://bodegasalianza.vteximg.com.br/arquivos/ids/157939-800-800/1804-1.jpg?v=636138051173200000';
     this.ginebraImg = 'https://s2.dia.es/medias/hb6/he4/9233199464478.jpg';
+    this.srcImg = this.ronImg;
+    this.drink = 'ron';
   }
 
   ngOnInit() {
   }
 
-  submitForm(myForm) {
-    if (myForm.value === 'ron') {
-      this.src = this.ronImg;
+  mix() {
+    console.log(this.drink)
+    if (this.drink === 'ron') {
+      this.srcImg = this.ronImg;
     }
-    if (myForm.value === 'vodka') {
-      this.src = this.vodkaImg;
+    if (this.drink === 'vodka') {
+      this.srcImg = this.vodkaImg;
     }
-    if (myForm.value === 'ginebra') {
-      this.src = this.ginebraImg;
+    if (this.drink === 'ginebra') {
+      this.srcImg = this.ginebraImg;
     }
   }
 }
